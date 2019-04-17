555975d040b6f5e83ac4f3df501861b2f4d4d682
Justin <jpreagan2013@gmail.com>
2019-04-17 18:39:05 -0400

    added buttons and color

--- etch.js
+++ etch.js
@@ -1,1 +1,76 @@
-const 
+let currentGrid = 16;
+
+createGrid(currentGrid);
+
+function createGrid(numberofSquares) {
+    let gridContainer = document.createElement('div');
+    gridContainer.className = "grid-container";
+    for (let i = 0; i < numberofSquares; i++) {
+        for (let j = 0; j < numberofSquares; j++) {
+            let gridItem = document.createElement('div');
+            gridItem.className = "item";
+            gridItem.style.height  = `${100/(numberofSquares)}%`; //hardcode
+            gridItem.style.width = `${100/(numberofSquares)}%`;
+            gridContainer.appendChild(gridItem);
+        }
+    }
+    document.body.appendChild(gridContainer);
+    changeColor(document.querySelectorAll('.item'));
+}
+
+function destroyGrid() {
+    let gridContainer = document.querySelector('.grid-container');
+    gridContainer.parentNode.removeChild(gridContainer);
+
+}
+
+function changeColor(itemList) {
+    itemList.forEach((item) => {
+        item.addEventListener('mouseover', (e) => {
+            e.target.style.background = 'black';
+        });
+    })
+}
+let btn = document.querySelector('#reset');
+
+btn.addEventListener('click', function(e){
+    destroyGrid();
+    createGrid(16);
+    changeColor(document.querySelectorAll('.item'));
+});
+
+btn = document.querySelector('#new-canvas');
+
+btn.addEventListener('click', function(e){
+    const newSize = prompt("Size?");
+    destroyGrid();
+    currentGrid = newSize;
+    createGrid(currentGrid);
+    changeColor(document.querySelectorAll('.item'));
+});
+
+btn = document.querySelector('#random-colors');
+
+btn.addEventListener('click', function(e){
+    destroyGrid();
+    createGrid(currentGrid);
+    setRandomColor(document.querySelectorAll('.item'));
+});
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  function setRandomColor(itemList) {
+    itemList.forEach((item) => {
+        item.addEventListener('mouseover', (e) => {
+            e.target.style.background = getRandomColor();
+        });
+    })
+  }
+
